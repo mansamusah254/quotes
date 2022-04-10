c67fb7cdfe9c64d9360bf3f6ad665cb13118eb62
mansamusah254 <mansamusah830@gmail.com>
2022-04-10 22:12:21 +0300

    like and dislike button customization

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -12,12 +12,7 @@ import { FormBuilder } from '@angular/forms';
 export class QuoteComponent implements OnInit {
   quotes:Quote[]=[
     new Quote('“The fool doth think he is wise, but the wise man knows himself to be a fool.” ',0, 0,'William Shakespeare'),
-    new Quote('the  mind in the body at peace',0, 0,'Alvin'),
-    new Quote('actuality is profanity',0, 0,'Alvin'),
-    new Quote('define insanity',0, 0,'Alvin'),
-    new Quote('live love life',0, 0,'Alvin'),
-    new Quote('audacity in profanity',0, 0,'Alvin'),
-    new Quote('git commit',0, 0,'Alvin'),
+
   ]
   constructor() { }
 
@@ -29,12 +24,13 @@ export class QuoteComponent implements OnInit {
   }
 
   numberOfLikes : number =0;
+  numberOfDislikes : number =0;
 
   likeButtonClick() {
     this.numberOfLikes++;
   }
 
   dislikeButtonClick() {
-    this.numberOfLikes--;
+    this.numberOfDislikes --;
   }
 }
