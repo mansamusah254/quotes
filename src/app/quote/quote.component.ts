b9e0b295f29d21ec88fd6c82cc8f1df1df7181c6
mansamusah254 <mansamusah830@gmail.com>
2022-04-10 11:18:03 +0300

    cards for the quotes

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -11,6 +11,12 @@ export class QuoteComponent implements OnInit {
     new Quote('the  mind in the body at peace',0, 0,'Alvin'),
     new Quote('actuality is profanity',0, 0,'Alvin'),
     new Quote('define insanity',0, 0,'Alvin'),
+    new Quote('live love life',0, 0,'Alvin'),
+    new Quote('the world is a village',0, 0,'Alvin'),
+    new Quote('live love life',0, 0,'Alvin'),
+    new Quote('the world is a village',0, 0,'Alvin'),
+    new Quote('audacity in profanity',0, 0,'Alvin'),
+    new Quote('git commit',0, 0,'Alvin'),
   ]
   constructor() { }
 
